e2000d8f6c7012406f9641000e365cf9b6883ea8
Edison Loor <edison.david.loor@gmail.com>
2022-07-25 18:16:07 -0500

    first commit

---
+++ CeibaChallenge/src/main/java/com/Ceiba/CeibaChallenge/CeibaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.Ceiba.CeibaChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaChallengeApplication.class, args);
+	}
+
+}
---
+++ CeibaChallenge/src/test/java/com/Ceiba/CeibaChallenge/CeibaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ceiba.CeibaChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeibaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
