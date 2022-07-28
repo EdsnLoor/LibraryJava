bebb2c920f8b1fc7488c7037ecec9d919658a4a9
Edison Loor <edison.david.loor@gmail.com>
2022-07-27 22:05:59 -0500

    get and put and delete added

--- CeibaChallenge/src/main/java/com/Ceiba/CeibaChallenge/dao/PersonDao.java
+++ CeibaChallenge/src/main/java/com/Ceiba/CeibaChallenge/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.Ceiba.CeibaChallenge.dao;
 import com.Ceiba.CeibaChallenge.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -13,4 +14,8 @@ public interface PersonDao {
         return insertPerson(id, person);
     }
     List<Person> selectAllPeople();
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById (UUID id);
+    int updatePersonById(UUID id, Person person);
 }
